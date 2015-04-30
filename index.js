d744edcc0c4d5f9d55af4537c58cb488682e8b1c
Joris Guyonvarch <joris.guyonvarch@gmail.com>
2015-04-30 14:56:59 +0200

    Calling multiple times the addPoint function keep previous points

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ if (!Detector.webgl) {
   globe.animate();
   document.body.style.backgroundImage = 'none'; // remove loading
 
-  addPoint(globe, 48.8567, 2.3508);
-  // addPoint(globe, 51.5062, 0.1275);
+  var subgeo = new THREE.Geometry();
+  var subgeo1 = addPoint(globe, 48.8567, 2.3508, subgeo);
+  var subgeo2 = addPoint(globe, 51.5062, 0.1275, subgeo1);
+  globe._baseGeometry = subgeo2;
   globe.createPoints();
 }
 
-function addPoint(globe, lat, lon) {
-  globe.is_animated = false;
-  var subgeo = new THREE.Geometry();
+function addPoint(globe, lat, lon, subgeo) {
+  var size = 100;
   var color = { r: 1, g: 1, b: 0 };
-  var size = 200;
   globe.addPoint(lat, lon, size, color, subgeo);
-  globe._baseGeometry = subgeo;
+  return subgeo;
 }
