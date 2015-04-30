6350c48f261d6b59f45f5dec88424907477bb3dd
Joris Guyonvarch <joris.guyonvarch@gmail.com>
2015-04-30 15:13:32 +0200

    Showing real opponenents position

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 var subgeo = new THREE.Geometry();
+var col = { r: 1, g: 1, b: 0 };
+var oCol = { r: 0.5, g: 0.5, b: 0.8 };
 
-if (!Detector.webgl) {
+if(!Detector.webgl) {
   Detector.addGetWebGLMessage();
+} else if(!window.EventSource) {
+  console.log('Event source is not available.');
 } else {
-
   var container = document.getElementById('container');
   var globe = new DAT.Globe(container);
   globe.animate();
-  document.body.style.backgroundImage = 'none'; // remove loading
 
-  addPoint(globe, 48.8567, 2.3508);
-  addPoint(globe, 51.5062, 0.1275);
+  var source = new EventSource("http://en.lichess.org/network/stream");
+
+  source.addEventListener('message', function(e) {
+    var lichessEvent = parseLichessEvent(e.data);
+    console.log(lichessEvent);
+    addPoint(globe, lichessEvent.lat, lichessEvent.lon, col);
+    addPoint(globe, lichessEvent.oLat, lichessEvent.oLon, oCol);
+  });
 }
 
-function addPoint(globe, lat, lon) {
+function addPoint(globe, lat, lon, color) {
   var size = 20;
-  var color = { r: 1, g: 1, b: 0 };
   globe.addPoint(lat, lon, size, color, subgeo);
   globe._baseGeometry = subgeo;
   globe.createPoints();
+}
+
+function parseLichessEvent(data) {
+  var raw = data.split('|');
+  var result = {
+    country: raw[0],
+    lat: parseFloat(raw[1]),
+    lon: parseFloat(raw[2]),
+    oLat: parseFloat(raw[3]),
+    oLon: parseFloat(raw[4])
+  };
+  result.lat += Math.random() - 0.5;
+  result.lon += Math.random() - 0.5;
+  return result;
 }
