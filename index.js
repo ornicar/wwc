b404d29c2465a17dd498f72220751efd8c5ba24b
Joris Guyonvarch <joris.guyonvarch@gmail.com>
2015-04-30 15:00:18 +0200

    Make it simpler to add multiple points

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var subgeo = new THREE.Geometry();
+
 if (!Detector.webgl) {
   Detector.addGetWebGLMessage();
 } else {
@@ -7,16 +9,14 @@ if (!Detector.webgl) {
   globe.animate();
   document.body.style.backgroundImage = 'none'; // remove loading
 
-  var subgeo = new THREE.Geometry();
-  var subgeo1 = addPoint(globe, 48.8567, 2.3508, subgeo);
-  var subgeo2 = addPoint(globe, 51.5062, 0.1275, subgeo1);
-  globe._baseGeometry = subgeo2;
-  globe.createPoints();
+  addPoint(globe, 48.8567, 2.3508);
+  addPoint(globe, 51.5062, 0.1275);
 }
 
-function addPoint(globe, lat, lon, subgeo) {
-  var size = 100;
+function addPoint(globe, lat, lon) {
+  var size = 20;
   var color = { r: 1, g: 1, b: 0 };
   globe.addPoint(lat, lon, size, color, subgeo);
-  return subgeo;
+  globe._baseGeometry = subgeo;
+  globe.createPoints();
 }
